9165c122c22145ff7a2da4d8f6a968d8f222a19f
SauloCouto <saulocouto1221@gmail.com>
2024-01-09 03:21:44 -0300

    Classes de entidade criadas e implementada seus atributos e tipos de dados

---
+++ Entities/Ask.ts
@@ -0,0 +1,56 @@
+import { DataTypes, Model } from "sequelize";
+import { DataBaseConfiguration } from "../Database/DataBaseConfiguration";
+
+const database: DataBaseConfiguration = new DataBaseConfiguration(
+    "guiaperguntas", 
+    "root", 
+    "admin#$SL", 
+    {host: "localhost", dialect: "mysql"}
+)
+
+database.getSequelize()
+    .sync()
+    .then(() => {
+        console.log("Table syncron success")
+    }).catch(err => {
+        throw new Error(err);
+    })
+
+class Ask extends Model {
+    private title!: string;
+    private description!: string;
+
+    public getTitle(): string {
+        return this.title;
+    }
+
+    public getDescription(): string {
+        return this.description;
+    }
+}
+
+Ask.init(
+    {
+        title: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                is: /^[\S*]/g
+            }
+        },
+
+        description: {
+            type: DataTypes.TEXT,
+            allowNull: false,
+            validate: {
+                is: /^[\S*]/g
+            }
+        }
+    },
+    {
+        sequelize: database.getSequelize(),
+        tableName: "tb_ask"
+    }
+)
+
+export default Ask
---
+++ Entities/Response.ts
@@ -0,0 +1,52 @@
+import { DataTypes, Model } from "sequelize";
+import { DataBaseConfiguration } from "../Database/DataBaseConfiguration";
+
+const database: DataBaseConfiguration = new DataBaseConfiguration(
+    "guiaperguntas", 
+    "root", 
+    "admin#$SL", 
+    {host: "localhost", dialect: "mysql"}
+)
+
+database.getSequelize()
+    .sync()
+    .then(() => {
+        console.log("Table syncron success")
+    }).catch(err => {
+        throw new Error(err);
+    })
+
+class ResponseAsk extends Model {
+    private body!: string;
+    private idAsk!: number;
+
+    public getBody(): string {
+        return this.body;
+    }
+
+    public getIdAsk(): number {
+        return this.idAsk;
+    }
+}
+
+ResponseAsk.init(
+    {
+        body: {
+            type: DataTypes.TEXT,
+            allowNull: false,
+            validate: {
+                is: /^[\S*]/g
+            }
+        },
+        idAsk: {
+            type: DataTypes.INTEGER,
+            allowNull: false
+        }
+    },
+    {
+        sequelize: database.getSequelize(),
+        tableName: "tb_response"
+    }
+)
+
+export default ResponseAsk

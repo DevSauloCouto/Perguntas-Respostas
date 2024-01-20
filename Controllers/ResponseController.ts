c46f911a015c51e8bce0fb392db55d63b7f57d44
SauloCouto <saulocouto1221@gmail.com>
2024-01-19 23:53:56 -0300

    rename variable the method 'listAllResponses'

--- Controllers/ResponseController.ts
+++ Controllers/ResponseController.ts
@@ -21,9 +21,9 @@ class ResponseController {
 
     async listAllResponses(req: Request, res: Response) {
         try {
-            const allResponses = await this.responseService.findAll();
+            const responses = await this.responseService.findAll();
 
-            res.status(200).json({allResponses})
+            res.status(200).json({responses})
         } catch(err) {
             res.status(404).json({error: err})
         }
@@ -31,7 +31,7 @@ class ResponseController {
 
     async listOneResponse(req: Request, res: Response) {
         const { id } = req.params;
-        const response: ResponseAsk|null = await this.responseService.findById(parseInt(id));
+        const response: ResponseAsk | null = await this.responseService.findById(parseInt(id));
 
         if(!response) {
             return res.status(404).json({error: `Response ${id} not found`});
